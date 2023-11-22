fbebba8943ad8e503216d64d5664e39d8b870332
Abdulwassa <abdu@gmail.com>
2023-11-23 00:05:50 +0300

    adding the code of js file

---
+++ script.js
@@ -0,0 +1,30 @@
+var day = 1;
+var dayName;
+
+switch (day) {
+  case 1:
+    dayName = 'Monday';
+    break;
+  case 2:
+    dayName = 'Tuesday';
+    break;
+  case 3:
+    dayName = 'Wednesday';
+    break;
+  case 4:
+    dayName = 'Thursday';
+    break;
+  case 5:
+    dayName = 'Friday';
+    break;
+  case 6:
+    dayName = 'Saturday';
+    break;
+  case 7:
+    dayName = 'Sunday';
+    break;
+  default:
+    dayName = 'day number week';
+}
+
+console.log('The day name is: ' + dayName);
